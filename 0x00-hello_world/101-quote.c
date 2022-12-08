dc7448de57214bd02bf538b4b7e056ce0f6dbbc3
Anthony Chuku <chukuanthony@gmail.com>
2022-12-08 09:05:20 -0800

    C program to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,10 @@
 /**
  * main - Receives no arguement value
  *
- * Return: Always 1 (Error)
+ * Return: Always 1
  */
-
 int main(void)
 {
-	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-	write(2,  err, 59);
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
 }
