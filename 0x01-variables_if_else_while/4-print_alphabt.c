5b7961e7d8e629da484d14d6f90cf26211bbbd8d
Anthony Chuku <chukuanthony@gmail.com>
2022-12-09 07:47:17 -0800

    prints all lower case letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Receives no arguement
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Receives no arguement
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
