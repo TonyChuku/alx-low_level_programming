740f3f28786a6a5fa2787db07a1f64657f0ae55c
Anthony Chuku <chukuanthony@gmail.com>
2022-12-13 06:59:28 -0800

    prints lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Returns no arguement
+ *
+ * Return: 0 success
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
