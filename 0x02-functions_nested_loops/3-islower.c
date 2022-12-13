003fa90a4b033e4b1e9dbaac3d461a33f6e1f278
Anthony Chuku <chukuanthony@gmail.com>
2022-12-13 09:49:04 -0800

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * checks for lowercase character
+ *
+ * Return: 1 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
