f50102e4ddefe66c6ac78a37eb4aa23fc7efdc68
Anthony Chuku <chukuanthony@gmail.com>
2022-12-19 05:43:24 -0800

    0-reset_to_98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,10 +4,8 @@
 /**
  * File: main.h
  * Author: Anthony Chuku
- * Description: Header file containing the prototypes of all functions and the prototype of the function _putchar
+ * Desc: Header file containing the prototypes of all functions and the prototype of the function _putchar
  */
-
-#include <stdlib.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
