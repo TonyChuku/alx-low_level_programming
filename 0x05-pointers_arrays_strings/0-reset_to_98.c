6004381f07024b95ea495392d3f874c7f8e8b7af
Anthony Chuku <chukuanthony@gmail.com>
2022-12-19 07:22:21 -0800

    pointers and arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,6 @@
 
 /**
  * reset_to_98 - change parameter value to 98
- * Description - a function that takes a pointer to an int 
  * as parameter and updates the value it points to to 98.
  * @n: The pointer to an int
  */
