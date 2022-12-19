3c4b5ed4ba94e09258f2ba3e03a5c75333c8ab00
Anthony Chuku <chukuanthony@gmail.com>
2022-12-19 05:59:03 -0800

    takes a pointer to an int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,9 @@
 
 /**
  * reset_to_98 - a function that takes a pointer to an int 
- * as parameter and updates the value it points to to 98.
+ *               as parameter and updates the value it points to to 98.
  * @n: The pointer to an int
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,8 +4,11 @@
 /**
  * File: main.h
  * Author: Anthony Chuku
- * Desc: Header file containing the prototypes of all functions and the prototype of the function _putchar
+ * Desc: Header file containing the prototypes of all functions 
+ *       and the prototype of the function _putchar.
  */
+
+#include <stdlib.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
