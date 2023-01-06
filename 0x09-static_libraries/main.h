af552ff49eb80ea128e9ad7eb00658c8fac45011
Anthony Chuku <chukuanthony@gmail.com>
2023-01-06 08:54:56 -0800

    static lib

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Anthony Chuku
- * Desc: Header file containing prototypes for all functions
- * used in the 0x09-static_libraries directory.
- */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
