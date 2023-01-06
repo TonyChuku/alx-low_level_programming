6913ddf146830c35559470abb719b6adf8428baa
Anthony Chuku <chukuanthony@gmail.com>
2023-01-05 23:07:07 -0800

    first commits

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Anthony Chuku
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0A-argc_argv directory.
+ */
+
+int _putchar(char c);
+
+#endif
+
