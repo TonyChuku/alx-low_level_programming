808ee1eba6908dee5a11c801c000566a31bca2a3
Anthony Chuku <chukuanthony@gmail.com>
2023-01-26 20:13:33 -0800

    first commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>
-int _putchar(char c);
+
 /**
- * struct list_s - singly linked lis
+ * File: lists.h
+ * Author: Anthony Chuku
+ * Description: Header file containing prototypes and definitions for all functions
+ * and types written in the 0x11-singly_linked_lists directory.
+ */
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
@@ -11,15 +19,17 @@ int _putchar(char c);
  * Description: singly linked list node structure
  * for ALX project
  */
-typedef struct list_s
+
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
